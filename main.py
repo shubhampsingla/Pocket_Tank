8f590a13f4c7b711d322d7dbd0bd2a230dc5776e
chiranjeev11 <chiranjeevkhurana11@gmail.com>
2020-08-23 21:17:58 +0530

    Added another tank and its control

--- main.py
+++ main.py
@@ -15,18 +15,29 @@ pygame.display.set_caption("Pocket Tank")
 # pygame.display.set_icon(icon)
 
 # Image of tank 
-image = pygame.image.load('icons8-tank-48.png')
-playerX = 400
-playerY = 300 
-playerX_change = 0
+image_1 = pygame.image.load('icons8-tank-48.png')
+playerX_1 = 200
+playerY_1 = 300 
+playerX_change_1 = 0
 
-def show_image(playerX, playerY):
+image_2 = pygame.image.load('icons8-tank-48.png')
+playerX_2 = 600
+playerY_2 = 300
+playerX_change_2 = 0
+
+def show_image_1(playerX_1, playerY_1):
 
 	# Drawing of image on screen (blit means to draw)
-	screen.blit(image, (playerX, playerY))
+	screen.blit(image_1, (playerX_1, playerY_1))
+
+def show_image_2(playerX_2, playerY_2):
+
+	screen.blit(image_2, (playerX_2, playerY_2))
 
 # Game loop
 running = True
+
+flag = 0
 
 while running:
 
@@ -41,21 +52,41 @@ while running:
 		# If Keystroke is pressed check whether it is left or right
 		if event.type == pygame.KEYDOWN:
 
-			if event.key == pygame.K_LEFT:
-				playerX_change = -0.1
+			if flag == 0:
 
-			if event.key == pygame.K_RIGHT:
-				playerX_change = 0.1
+				if event.key == pygame.K_LEFT:
+					playerX_change_1 = -0.1
+
+				if event.key == pygame.K_RIGHT:
+					playerX_change_1 = 0.1
+
+			else:
+				if event.key == pygame.K_LEFT:
+					playerX_change_2 = -0.1
+
+				if event.key == pygame.K_RIGHT:
+					playerX_change_2 = 0.1
+
+			if event.key == pygame.K_RETURN:
+				if flag==0:
+					flag=1
+				else:
+					flag=0
 					
 		if event.type == pygame.KEYUP:
 
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				playerX_change = 0
+				playerX_change_1 = 0
+				playerX_change_2 = 0
 
-	playerX += playerX_change
+	playerX_1 += playerX_change_1
+
+	playerX_2 += playerX_change_2
 
 	# Tank image
-	show_image(playerX, playerY)
+	show_image_1(playerX_1, playerY_1)
+
+	show_image_2(playerX_2, playerY_2)
 
 	# To update the display
 	pygame.display.update()
